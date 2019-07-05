7a0f125fd642a1215e864c4dc22a844863aaa687
Misha <mikhail.kouznetsov@gmail.com>
2019-07-05 14:09:03 -0700

    Init

---
+++ SpaceXLaunchSchedule/ViewControllers/TableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  SpaceXLaunchSchedule
+//
+//  Created by Mikhail Kouznetsov on 7/5/19.
+//  Copyright © 2019 Mikhail Kouznetsov. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+    
+    lazy var headerView:UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [ headerView, tableView])
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+        stackView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        return stackView
+    }()
+    
+    lazy var tableView:UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .white
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.contentInsetAdjustmentBehavior = .never
+        tableView.keyboardDismissMode = .onDrag
+        tableView.scrollsToTop = true
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        setLayout()
+    }
+    
+    func setLayout(){
+        stackView.frame = view.bounds
+        view.addSubview(stackView)
+        headerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3).isActive = true
+    }
+}
+
+extension TableViewController:UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    
+}
+
